0bae662c430c00afe83962729d7fde9dc1cbea57
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2019-05-19 19:49:21 +0300

    firt commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import * as d3 from 'd3';
+
+const canvas = document.querySelector('canvas');
+
+const context = canvas.getContext("2d");
+const width = canvas.width;
+const height = canvas.height;
+
+
