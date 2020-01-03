9c8b52adb871a89b3542a774e20993b79b10a256
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2020-01-04 00:38:07 +0300

    add menu

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div :class="$style.root">
-    <x-cubic
-      v-if="calendarFrom && calendarTo && calendarStep"
-      cubic-id="root"
-      :from="calendarFrom"
-      :to="calendarTo"
-      :step="calendarStep"
-      color="white"
-      :class="$style.calendar"
-    />
+    <x-menu @active="active = $event"/>
+    <div v-if="active === 'calendar'" :class="$style.content">
+      <x-cubic
+        v-if="calendarFrom && calendarTo && calendarStep"
+        cubic-id="root"
+        :from="calendarFrom"
+        :to="calendarTo"
+        :step="calendarStep"
+        color="white"
+        :class="$style.calendar"
+      />
 
-    <x-legend />
+      <x-legend />
+    </div>
   </div>
 </template>
 
@@ -19,18 +22,21 @@ import moment from 'moment';
 import { mapState, mapActions } from 'vuex';
 import { calendar as dbCalendar, events as dbEvents } from './db';
 import XCubic from 'blocks/x-cubic/x-cubic.vue';
+import XMenu from 'blocks/x-menu/x-menu.vue';
 import XLegend from 'blocks/x-legend/x-legend.vue';
 
 export default {
   components: {
     XCubic,
     XLegend,
+    XMenu,
   },
   props: {
   },
   data() {
     return {
       events: dbEvents,      
+      active: false,
     };
   },
   computed: {
@@ -73,7 +79,7 @@ export default {
     background: black;
   }
 
-  .root {
+  .content {
     display: flex;
   }
 
---
+++ src/components/x-menu/x-menu.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="$style.root">
+    <span
+      v-for="item in menu"
+      :class="computedClass(item)"
+      @click="select(item)"
+    >
+      {{ item }}
+    </span>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+  },
+  props: {
+  },
+  data() {
+    return {
+      activeMenu: 'calendar',
+      menu: ['calendar', 'controls'],
+    };
+  },
+  mounted() {
+    this.$emit('acitve', this.activeMenu);
+  },
+  methods: {
+    select(menuName) {
+      this.activeMenu = menuName;
+      this.$emit('active', menuName);
+    },
+    computedClass(item) {
+      return {
+        [this.$style.menu]: true,
+        [this.$style.activeMenu]: item === this.activeMenu,
+      };
+    },
+  },
+};
+</script>
+
+<style module>
+  .root {
+    display: block;
+    color: white;
+    border: 1px solid white;
+    padding: 4px 10px;
+    margin-bottom: 10px;
+    cursor: pointer;
+  }
+
+  .root > .menu + .menu {
+    margin-left: 4px;
+  }
+
+  .activeMenu {
+    color: red;
+  }
+</style>
